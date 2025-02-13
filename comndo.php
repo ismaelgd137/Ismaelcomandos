98fe3c430f495f1427873614c1810e6908a61007
ismaelgd137 <wazaaaaa333333@gmail.com>
2025-02-13 20:30:33 -0300

    Add files via upload

---
+++ comndo.php
@@ -0,0 +1,49 @@
+
+<?php
+    $respuestas = array(
+     
+        "La suerte te sonríe hoy.",
+        "Un nuevo comienzo te espera.",
+        "La fortuna te favorece.",
+        "Tu destino es brillante.",
+        "La perseverancia te llevará al éxito.",
+        "Un amigo inesperado te ayudará.",
+        "La oportunidad llama a tu puerta.",
+        "Tu corazón te guiará hacia la felicidad.",
+        "La confianza en ti mismo te hará triunfar.",
+        "Un cambio inesperado te beneficiará.",
+        "La paciencia te llevará a la victoria.",
+        "Tu intuición es más fuerte de lo que crees.",
+        "La suerte te acompañará en tu camino.",
+        "Un regalo inesperado te llegará pronto.",
+        "Tu creatividad te llevará al éxito.",
+        "La honestidad te abrirá puertas.",
+        "Un viaje inesperado te cambiará la vida.",
+        "La generosidad te traerá recompensas.",
+        "Tu pasión te llevará a la realización.",
+        "La tranquilidad te encontrarás en la naturaleza.",
+        "Un encuentro casual te cambiará la vida.",
+        "La determinación te hará superar obstáculos.",
+        "Tu sonrisa iluminará el camino.",
+        "La gratitud te traerá paz interior.",
+        "Un nuevo hobby te apasionará.",
+        "La intuición te guiará hacia la sabiduría.",
+        "La amistad te brindará apoyo incondicional.",
+        "Un cambio de actitud te transformará.",
+        "La curiosidad te llevará a descubrimientos increíbles.",
+        "Tu corazón te guiará hacia el amor.",
+        "La responsabilidad te hará crecer.",
+        "Un consejo sabio te ayudará a tomar decisiones.",
+        "La imaginación te llevará a lugares increíbles.",
+        "La humildad te hará grande.",
+        "Un regalo de la vida te sorprenderá.",
+        "La fe te llevará a través de momentos difíciles.",
+        "Tu voz será escuchada.",
+        "La conexión con la naturaleza te renovará.",
+        "Un nuevo capítulo en tu vida está a punto de comenzar.",
+        "La alegría te acompañará en tu camino."
+        );
+    $random = array_rand($respuestas);
+    echo $respuestas[$random];
+    ?>
+                            
